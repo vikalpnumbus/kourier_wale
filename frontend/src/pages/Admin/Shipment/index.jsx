3b994fcd237cb689ea484ac3d0450546f6f4f046
rajsingh0187 <=>
2025-12-25 12:13:58 +0530

    shipment

--- frontend/src/pages/Admin/Shipment/index.jsx
+++ frontend/src/pages/Admin/Shipment/index.jsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import Icon from '@mdi/react';
 import { mdiClose, mdiFilterOutline } from '@mdi/js';
 import { Outlet } from "react-router-dom";
+import OrdersFilter from "../../Seller/Orders/OrdersFilter";
 
 function Shipment() {
-    const [showFilters, setShowFilters] = useState(false);
+  const [showFilters, setShowFilters] = useState(false);
   return (
     <div className="row">
       <div className="col-md-12 grid-margin stretch-card d-md-flex">
@@ -17,12 +18,12 @@ function Shipment() {
                 </h4>
               </div>
               <div className="col-md-8 col-sm-12 d-flex justify-content-end gap-2">
-                
-                
+
+
                 {!location.pathname.includes("/shipment/edit") &&
                   !location.pathname.includes("/shipment/view") && (
                     <button
-                    //   onClick={() => setShowFilters(!showFilters)}
+                      onClick={() => setShowFilters(!showFilters)}
                       type="button"
                       className="btn btn-dark btn-md py-2 px-4"
                     >
@@ -38,11 +39,11 @@ function Shipment() {
                     </button>
                   )}
               </div>
-              {/* {!location.pathname.includes("/orders/edit") &&
+              {!location.pathname.includes("/orders/edit") &&
                 !location.pathname.includes("/orders/add") &&
                 !location.pathname.includes("/orders/view") &&
                 !location.pathname.includes("/orders/clone") &&
-                showFilters && <OrdersFilter setShowFilters={setShowFilters} />} */}
+                showFilters && <OrdersFilter setShowFilters={setShowFilters} />}
             </div>
 
             <div className="row mt-3">
@@ -53,7 +54,7 @@ function Shipment() {
           </div>
         </div>
       </div>
-     
+
     </div>
   );
 }
