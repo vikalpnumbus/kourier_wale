128ce2d0342a78e5d8b9f26fafcf57b898b78c1c
Rahul Singh <rahul.singh@gmail.com>
2026-01-12 03:31:00 +0530

    WIP INVOCIE

--- backend/services/invoiceSettings.services.mjs
+++ backend/services/invoiceSettings.services.mjs
@@ -1,4 +1,11 @@
+import { Op } from "sequelize";
 import FactoryRepository from "../repositories/factory.repository.mjs";
+import CompanyDetailsService from "./companyDetails.service.mjs";
+import OrdersService from "./orders.service.mjs";
+import CourierService from "./courier.service.mjs";
+import WarehouseService from "./warehouse.service.mjs";
+import ProductsService from "./products.service.mjs";
+import UserService from "./user.service.mjs";
 
 class Service {
   constructor() {
@@ -61,10 +68,50 @@ class Service {
   }
   async generate({ userId, shipping_db_ids }) {
     try {
-      const result = await this.shippingRepository.find({ userId, id: shipping_db_ids, [Op.notIn]: ["cancelled", "new"] });
-      console.log("result: ", result);
+      const result = await this.shippingRepository.find({ userId, id: shipping_db_ids, shipping_status: { [Op.notIn]: ["cancelled", "new"] } });
 
       if (!result) throw new Error("Some error occured. Please try again later.");
+      result.map(async (shipment) => {
+        const { order_db_id, warehouse_id, courier_id, shipping_status, awb_number, createdAt, products } = shipment;
+        console.log("{order_db_id, warehouse_id, courier_id, shipping_status, awb_number, createdAt} : ", {
+          order_db_id,
+          warehouse_id,
+          courier_id,
+          shipping_status,
+          awb_number,
+          createdAt,
+        });
+
+        const [
+          orderDataRes = {},
+          companyDetailsDataRes = {},
+          courierDataRes = {},
+          warehouseDataRes = {},
+          productsDataRes = {},
+          invoiceSettingsDataRes = {},
+          orderInvoiceDateDataRes = {},
+          userDataRes = {},
+        ] = await Promise.all([
+          await OrdersService.read({ userId, id: order_db_id }),
+          await CompanyDetailsService.view({ id: userId }),
+          await CourierService.read({ id: courier_id }),
+          await WarehouseService.read(warehouse_id == "0" ? { userId } : { id: warehouse_id, userId }),
+          products,
+          await this.repository.findOne({ userId }),
+          {},
+          await UserService.read({ id: userId }),
+        ]);
+
+        const orderData = orderDataRes?.data?.result?.[0];
+        const companyDetailsData = companyDetailsDataRes?.data?.companyDetails;
+        const courierData = courierDataRes?.data?.result?.[0];
+        const warehouseData = warehouseDataRes?.data?.result?.[0];
+        const productsData = orderData?.products;
+        const invoiceSettingsData = invoiceSettingsDataRes;
+        const orderInvoiceDateData = orderInvoiceDateDataRes?.data?.result?.[0];
+        const userData = userDataRes
+        console.log({ orderData, companyDetailsData, courierData, warehouseData, productsData, invoiceSettingsData, orderInvoiceDateData, userData });
+      });
 
       return {
         status: 201,
