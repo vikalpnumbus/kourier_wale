9fd0693adf1c7261f058a019ba8ace2335f4d9c1
Vikalp Sharma <sharmavikalp99@gmail.com>
2026-02-27 20:49:22 +0530

    Code Push to Master

--- frontend/src/config/RemittanceDetails/RemittanceConfig.jsx
+++ frontend/src/config/RemittanceDetails/RemittanceConfig.jsx
@@ -1,6 +1,6 @@
 
 const apiUrl = import.meta.env.VITE_API_URL;
 const RemittanceConfig = {
-  remittance_list: apiUrl + "remittance/seller",
+  remittance_list: apiUrl + "/remittance/seller",
 };
 export default RemittanceConfig;
