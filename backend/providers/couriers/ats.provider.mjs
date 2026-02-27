53675d31e4b77431f2b9e9612327af02a111c331
Vikalp Sharma <sharmavikalp99@gmail.com>
2026-02-27 07:53:04 +0530

    code push

--- backend/providers/couriers/ats.provider.mjs
+++ backend/providers/couriers/ats.provider.mjs
@@ -144,9 +144,9 @@ class ATSProvider {
         }
       }
       console.log("Final Payload of shipments", payload);
-      console.log("create shipment url", ATS_CREATE_SHIPMENT_FORWARD);
+      console.log("create shipment url", "https://sandbox.sellingpartnerapi-eu.amazon.com/shipping/v2/oneClickShipment");
       const response = await axios.post(
-        ATS_CREATE_SHIPMENT_FORWARD,
+        "https://sandbox.sellingpartnerapi-eu.amazon.com/shipping/v2/oneClickShipment",
         payload,
         {
           headers: {
