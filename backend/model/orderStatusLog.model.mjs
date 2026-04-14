e9c957cacf2946fdc1c55e0b3b7fd7baac51a0d4
Vikalp Sharma <sharmavikalp99@gmail.com>
2026-04-14 22:38:34 +0530

    fdmfkmskfm

--- backend/model/orderStatusLog.model.mjs
+++ backend/model/orderStatusLog.model.mjs
@@ -1,6 +1,5 @@
 import { DataTypes } from "sequelize";
 import sqlDB from "../configurations/sql.config.mjs";
-
 const OrderStatusLog = sqlDB.sequelize.define(
   "order_status_logs",
   {
@@ -9,32 +8,26 @@ const OrderStatusLog = sqlDB.sequelize.define(
       primaryKey: true,
       autoIncrement: true,
     },
-
+    shipment_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     user_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
-
-    order_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-
     awb_number: {
       type: DataTypes.STRING,
       allowNull: true,
     },
-
     courier_name: {
       type: DataTypes.STRING,
       allowNull: true,
     },
-
     ship_status: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-
     raw_payload: {
       type: DataTypes.JSON,
       allowNull: true,
@@ -45,5 +38,4 @@ const OrderStatusLog = sqlDB.sequelize.define(
     timestamps: true,
   }
 );
-
 export default OrderStatusLog;
